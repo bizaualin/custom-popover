54e85aecbbdfb8af6f91c935e24553a9161cb625
Alin Bizau <bizau.alin@yahoo.com>
2020-06-08 20:05:06 +0300

    Create project and add login form

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public formGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  public login(): void {
+    console.log(this.formGroup.value);
+  }
+}
